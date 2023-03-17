08200e17abc354d7fd8afd058a2f372c2535e866
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-03-17 23:01:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main{
+	public static void main(String[]args){
+		//initialization"int =number1",condition,loop body"number<=10;",update"number++""
+		//start with 1 and it will be true since 1 is <=10,then it will update and continue untill it's not true/reach 10. 
+		int numbers[]={1,2,3,4,5,6,7,8,9,10};
+		//added index which starts at 1, so everything will be printed except the 1.
+		for(int index=1; index < numbers.length; index++){
+			//we can add all our numbers in the idnex/length together to get the sum aswell
+			System.out.println(numbers[index]);
+			
+		
+			
+	
+			
+		}
+		}
+	}	
+
